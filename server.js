efb0207cdf09a700765375502ebd632a1b058274
Tom Nicholas <tomblyth90@gmail.com>
2023-03-17 13:28:26 +1100

    memorystore debugged

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express")
 const app = express()
+const port = process.env.PORT || 8080
+const session = require("express-session")
+const MemoryStore = require("memorystore")(session)
+
+const setCurrentUser = require('./middlewares/setCurrentUser.js')
+const viewHelpers = require("./middlewares/view_helpers.js")
 const methodOverride = require('./middlewares/method_override.js')
 const sessionController = require("./controllers/session_controller.js")
 const learningController = require("./controllers/learning_controller.js")
 const logger = require('./middlewares/logger.js')
-const session = require('express-session')
-const MemoryStore = require("memorystore")(session)
-const setCurrentUser = require('./middlewares/setCurrentUser.js')
-const viewHelpers = require("./middlewares/view_helpers.js")
+
+
+
 const userController = require('./controllers/user_controller')
 const notesController = require('./controllers/notes_controller')
 
-const port = process.env.PORT || 3000
+
 
 app.set("view engine", "ejs")
 
@@ -23,17 +28,17 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use("/",  methodOverride)
 
-app.use(session({
-    cookie: { maxAge: 8640000},
-    store: new MemoryStore({
-      checkPeriod: 8640000
-    })},
-    {
+app.use(
+    session({
+      cookie: { maxAge: 86400000 },
+      store: new MemoryStore({
+        checkPeriod: 86400000, // prune expired entries every 24h
+      }),
       secret: process.env.SESSION_SECRET || "keyboard cat",
       resave: false,
-      saveUninitalized: true,
-  }))
-
+      saveUninitialized: true,
+    })
+  )
 app.use(setCurrentUser)
 
 app.use(viewHelpers)
